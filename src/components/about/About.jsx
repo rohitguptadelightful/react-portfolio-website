a276b2679332379a7fc32d07368199b19697ee55
rohitguptadelightfull <contact.teamrkg@gmail.com>
2022-08-09 19:39:29 +0530

    changes in gitignore

---
+++ src/components/about/About.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./about.css"
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='About Image' />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>1+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>30+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Hello there! I'm a Specially Expert in Fb & Google Ads and Web Development, and I'm Very Passionate and Dedicated to My Work.
+          </p>
+          <p>
+            With 1 years of experience as a professional Advertiser & Web Developer, I have acquired the skills and knowledge necessary to make your projects a success. I enjoy every step of the advertising process, from discussion and collaboration.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
